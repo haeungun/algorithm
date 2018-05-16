12b5b5727d1e02d1ecfe61acdac94a8743f104f4
Haeun Kim <haeungun@gmail.com>
2018-05-16 10:36:57 +0900

    Create Tiling.java

---
+++ algospot/Tiling.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+
+public class Main {
+	
+	public static int tiling(int n) {
+		if (n == 1) return 1;
+		if (n == 2) return 2;
+		
+		int[] nums = new int[n];
+		nums[0] = 1;
+		nums[1] = 2;
+		
+		for (int i = 2; i < n; i++) {
+			nums[i] = (nums[i - 1] + nums[i - 2]) % 1000000007;
+		}
+		
+		return nums[n - 1];
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int numsOfTestCase = scan.nextInt();
+		int[] results = new int[numsOfTestCase];
+		
+		for (int i = 0; i < numsOfTestCase; i++) {
+			results[i] = tiling(scan.nextInt());
+		}
+		
+		for (int i = 0; i < numsOfTestCase; i++) {
+			System.out.println(results[i]);
+		}
+	}
+}
