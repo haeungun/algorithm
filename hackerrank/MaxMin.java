41fd30261fc3b7b09ddd9bd0909ec89aa41933bd
Haeun Kim <haeungun@gmail.com>
2019-02-19 21:21:15 +0900

    Create MaxMin.java

---
+++ hackerrank/MaxMin.java
@@ -0,0 +1,56 @@
+/**
+ * https://www.hackerrank.com/challenges/angry-children/problem?
+ */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int maxMin(int k, int[] arr) {
+        Arrays.sort(arr);
+        
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i <= arr.length - k; i++) {
+            int[] sub = Arrays.copyOfRange(arr, i, i + k);
+            int max = sub[sub.length - 1];
+            int min = sub[0];
+            result = Math.min(result, max - min);
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int k = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            arr[i] = arrItem;
+        }
+
+        int result = maxMin(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
